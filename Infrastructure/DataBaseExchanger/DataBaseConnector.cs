4b783419c8e3ce21310f45df54a15edaed507392
RWThirTeen <roger.mail.1995@gmail.com>
2025-06-19 22:32:11 +0800

    Создание View и ViewModel для меню

---
+++ Infrastructure/DataBaseExchanger/DataBaseConnector.cs
@@ -0,0 +1,45 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BudgetApp.Infrastructure.DataBaseExchanger
+{
+    internal class DataBaseConnector
+    {
+        //MySqlConnection MySQLConnection = new MySqlConnection
+        //    (@"Server=server242.hosting.reg.ru; 
+        //    Port=3306; 
+        //    Database=u1759854_rj45_db; 
+        //    User=u1759854_user; 
+        //    Password=UserPassword13;
+        //    charset=cp1251;
+        //    Allow Zero DateTime = true;
+        //    SSL Mode = Required");
+
+        //public void openConnection()
+        //{
+
+        //    if (MySQLConnection.State == System.Data.ConnectionState.Closed)
+        //    {
+        //        MySQLConnection.Open();
+        //    }
+        //}
+
+        //public void closeConnection()
+        //{
+        //    if (MySQLConnection.State == System.Data.ConnectionState.Open)
+        //    {
+        //        MySQLConnection.Close();
+        //    }
+        //}
+
+        //public MySqlConnection GetConnection()
+        //{
+        //    return MySQLConnection;
+        //}
+    }
+}
---
+++ ViewModels/MainMenuViewModel.cs
@@ -0,0 +1,82 @@
+﻿using BudgetApp.Infrastructure.Commands;
+using BudgetApp.ViewModels.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BudgetApp.ViewModels
+{
+    internal class MainMenuViewModel : ViewModel
+    {
+        readonly MainWindowViewModel _mainWindowVM;
+
+
+
+
+
+
+
+        #region Commands
+
+        public Command OpenStatisticMenuCommand { get; }
+        bool CanOpenStatisticMenuCommandExecute (object p) => true;
+        void OnOpenStatisticMenuCommandExecuted (object p)
+        {
+
+        }
+
+        public Command OpenIncomeMenuCommand { get; }
+        bool CanOpenIncomeMenuCommandExecute(object p) => true;
+        void OnOpenIncomeMenuCommandExecuted(object p)
+        {
+
+        }
+
+        public Command OpenOutcomeMenuCommand { get; }
+        bool CanOpenOutcomeMenuCommandExecute(object p) => true;
+        void OnOpenOutcomeMenuCommandExecuted(object p)
+        {
+
+        }
+
+        public Command OpenActivesMenuCommand { get; }
+        bool CanOpenActivesMenuCommandExecute (object p) => true;
+        void OnOpenActivesMenuCommandExecuted (object p)
+        {
+
+        }
+
+        public Command OpenSettingsMenuCommand { get; }
+        bool CanOpenSettingsMenuCommandExecute (object p) => true;
+        void OnOpenSettingsMenuCommandExecuted (object p)
+        {
+
+        }
+
+        public Command ExitApplicationCommand { get; }
+        bool CanExitApplicationCommandExecute(object p) => true;
+        void OnExitApplicationCommandExecuted(object p)
+        {
+            App.Current.Shutdown();
+        }
+
+        #endregion
+
+
+        public MainMenuViewModel(MainWindowViewModel mainWindowVM)
+        {
+            _mainWindowVM = mainWindowVM;
+
+
+
+            OpenStatisticMenuCommand = new LambdaCommand(OnOpenStatisticMenuCommandExecuted, CanOpenStatisticMenuCommandExecute);
+            OpenIncomeMenuCommand = new LambdaCommand(OnOpenIncomeMenuCommandExecuted, CanOpenIncomeMenuCommandExecute);
+            OpenOutcomeMenuCommand = new LambdaCommand(OnOpenOutcomeMenuCommandExecuted, CanOpenOutcomeMenuCommandExecute);
+            OpenActivesMenuCommand = new LambdaCommand(OnOpenActivesMenuCommandExecuted, CanOpenActivesMenuCommandExecute);
+            OpenSettingsMenuCommand = new LambdaCommand(OnOpenSettingsMenuCommandExecuted, CanOpenSettingsMenuCommandExecute);
+            ExitApplicationCommand = new LambdaCommand(OnExitApplicationCommandExecuted, CanExitApplicationCommandExecute);
+        }
+    }
+}
